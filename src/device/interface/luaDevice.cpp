f1383f468b4b8ae61518d2114d74629dd73e2c4e
Pascal Pieper <accounts@pascalpieper.de>
2022-11-02 19:37:35 +0100

    Correct custom constructor for Pixel

--- src/device/interface/luaDevice.cpp
+++ src/device/interface/luaDevice.cpp
@@ -285,7 +285,7 @@ void LuaDevice::Graphbuf_Interface_Lua::declarePixelFormat(lua_State* L) {
 		luabridge::getGlobalNamespace(L)
 			.beginNamespace("graphbuf")
 			  .beginClass <Pixel> ("Pixel")
-			    //.addConstructor <void (*) (const uint8_t, const uint8_t, const uint8_t, const uint8_t)> ()
+			    .addConstructor (+[](void* ptr, const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a) { return new (ptr) Pixel{r,g,b,a};})
 			    .addProperty ("r", &Pixel::r)
 			    .addProperty ("g", &Pixel::g)
 			    .addProperty ("b", &Pixel::b)
