44ca2c9bde401b224dc9ad2368212792dcdeb61f
Karl Aaron Rudkowski <rudkowsk@uni-bremen.de>
2022-12-06 11:22:28 +0100

    Pin Dialog: Remove Pins.
    Pin dialog only returns changed connections. If old pin was also valid, it is removed before adding new pin connection.
    Through this, pins can also be deleted by giving an invalid value (e.g. 255). For this, the TODOs in breadboard-connections need to be finished first.

--- src/breadboard/dialog/pins.cpp
+++ src/breadboard/dialog/pins.cpp
@@ -20,15 +20,19 @@ void PinDialog::addPin(Device::PIN_Interface::DevicePin device_pin, gpio::PinNum
     box->setWrapping(true);
     box->setValue(global);
     connect(box, QOverload<int>::of(&QSpinBox::valueChanged), [this, device_pin](int newValue) {
-       m_globals.erase(device_pin);
-       m_globals.emplace(device_pin, newValue);
+        auto current_pin = m_globals_input.find(device_pin);
+        if(current_pin != m_globals_input.end() && current_pin->second != newValue) {
+            m_globals_output.erase(device_pin);
+            m_globals_output.emplace(device_pin, newValue);
+        }
     });
     m_layout->insertRow(0, QString::number(device_pin), box);
 }
 
 void PinDialog::setPins(DeviceID device_id, std::unordered_map<Device::PIN_Interface::DevicePin, gpio::PinNumber> globals) {
     m_device = device_id;
-    m_globals = globals;
+    m_globals_input = globals;
+    m_globals_output.clear();
     for(auto const& [device_pin, global] : globals) {
         addPin(device_pin, global);
     }
@@ -38,10 +42,11 @@ void PinDialog::removePins() {
     while(m_layout->rowCount() > 1) {
         m_layout->removeRow(0);
     }
-    m_globals.clear();
+    m_globals_input.clear();
+    m_globals_output.clear();
     m_device = "";
 }
 
 void PinDialog::accept() {
-    emit(pinsChanged(m_device, m_globals));
+    emit(pinsChanged(m_device, m_globals_output));
 }
